a926181ad903067730c65c3b6e10687a3c2b55bf
Joaquim-Salvador <joaquimlennertz@gmail.com>
2026-02-03 17:42:23 -0300

    Cria uma entidade no banco de dados apartir do PessoaModel

---
+++ src/main/java/dev/Joaquim/CadastroDePessoas/PessoaModel.java
@@ -0,0 +1,67 @@
+package dev.Joaquim.CadastroDePessoas;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+// Entity transforma uma classe em uma entidade do banco de dados
+// JPA = Java Persistence API
+@Entity
+@Table(name = "tb_cadastro")
+public class PessoaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String cpf;
+    private String corFavorita;
+
+        public PessoaModel() {
+    }
+
+        public PessoaModel(String nome, String email, String cpf, String corFavorita) {
+            this.nome = nome;
+            this.email = email;
+            this.cpf = cpf;
+            this.corFavorita = corFavorita;
+        }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCorFavorita() {
+        return corFavorita;
+    }
+
+    public void setCorFavorita(String corFavorita) {
+        this.corFavorita = corFavorita;
+    }
+
+   
+    
+}
